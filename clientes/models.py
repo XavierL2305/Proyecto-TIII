2cf6ba23b2e7d0f3859cbf31574886fa850dc2ca
asvg1812 <ngelvera8@gmail.com>
2025-10-11 14:16:33 -0400

    mejoras perfil y restricciones segun su rol

--- clientes/models.py
+++ clientes/models.py
@@ -5,6 +5,7 @@ class ClienteProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     telefono = models.CharField(max_length=20, blank=True, null=True, verbose_name="Teléfono")
     direccion = models.CharField(max_length=255, blank=True, null=True, verbose_name="Dirección")
+    fecha_nacimiento = models.DateField(blank=True, null=True, verbose_name="Fecha de nacimiento")
 
     def __str__(self):
         return f"Perfil de {self.user.username}"
